b456d02a67f4a091ec8f6dfd7d3fb918b91f3124
yeshen97 <123631649+yeshen97@users.noreply.github.com>
2023-04-24 15:22:00 +0800

    pull commit

--- src/main/java/com/nanchang/git/GitTest.java
+++ src/main/java/com/nanchang/git/GitTest.java
@@ -2,8 +2,10 @@ package com.nanchang.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf( "hello git");
-        System.out.printf( "hello git2");
-        System.out.printf( "hello git3");
+        System.out.printf( " git");
+        System.out.printf( " git2");
+        System.out.printf( " git3");
+         System.out.printf( " git4");
+         System.out.printf( " git5");
     }
 }
